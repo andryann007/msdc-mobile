096df5217e4918a666d44a1c3cfcd51c282a8d34
Andryan <andryancoolz@gmail.cpm>
2022-11-15 21:57:25 +0700

    MSDC

--- app/src/main/java/com/example/msdc/SplashActivity.java
+++ app/src/main/java/com/example/msdc/SplashActivity.java
@@ -1,14 +1,28 @@
 package com.example.msdc;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        //start main screen after 2seconds
+        new Handler().postDelayed(this::login,2500);//2500 means 2.5 seconds
+    }
+
+    private void login() {
+        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        finish();
     }
 }
